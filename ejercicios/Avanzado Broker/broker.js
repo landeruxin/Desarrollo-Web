42059accfa147fba53220cfba1919bf2d49e7400
landeruxin <60647969+landeruxin@users.noreply.github.com>
2021-01-25 18:21:25 +0100

    Add files via upload

--- ejercicios/Avanzado Broker/broker.js
+++ ejercicios/Avanzado Broker/broker.js
@@ -1,90 +1,178 @@
-    var contador = 100;
+    var precio = 100;
 
     var capital = 1000;
 
     var accion = 0;
 
-    var valor = accion * contador;
+    var valor = accion * precio;
+
+    var total;
+
+
+    var precio_viejo;
+    
+
+
+
+    
 
 function inicio(){
 
+    setTimeout(function(){fin()},30000)
+
+    empezarAleatorio();
     
-    var nombre = prompt("Introduzca su nombre:");
+    var nombre = prompt("Bienvenido al juego, recuerde que la duración de este es de 10 segundos. \n\nIntroduzca su nombre:");
 
-    var frase = "Jugador: "+nombre
+    var frase = "Jugador: "+nombre;
 
-    document.getElementById('nombre').innerHTML='<p>'+frase+'</p>';
-
-    
-
-    document.getElementById('v1').innerHTML=capital;
-
-    document.getElementById('v2').innerHTML=contador;
-
-    document.getElementById('v3').innerHTML=accion;
-
-    document.getElementById('v4').innerHTML=valor;
+    document.getElementById('nombre').innerHTML='<p>'+frase+'</p>';    
 
 }
 
 
-function random(min, max) {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
+function reinicio(){
+
+    setTimeout(function(){fin()},30000)
+
+    empezarAleatorio();
+  
+
 }
 
-setTimeout(contador=contador*random(-50,50),5000);
+    
+
+
+function empezarAleatorio(){
+    	
+        setInterval(function(){random1()},1000); 
+    	
+    }
+
+
+
+function random1() {
+    var max = precio * 1.5;
+    var min = precio * 0.5;
+    var resultado = Math.floor((Math.random() * (max - min + 1)) + min);
+
+    precio = resultado;
+
+
+
+    pintar_resultado(precio);
+    pintar();
+
+
+    document.getElementById('cambio').innerHTML=''; 
+    if(precio > precio_viejo){
+        document.getElementById('cambio').innerHTML='<img class="cambio" src="subida.png">';  
+        
+    }else{
+        document.getElementById('cambio').innerHTML='<img class="cambio" src="bajada.png">';
+    }
+
+    precio_viejo = precio;
+    
+
+    return precio;
+
+    
+    
+}
+
+
 
 
 function comprar(){
 
 
 
-    if(capital>=contador){
-        v3 = v3 +1;
-        v2 = v2 - v1;
-        v4 = v3 * v1;
+    if(capital>=precio){
+        accion  = accion +1;
+        capital = capital - precio;
+        valor = accion * precio;
     }
 
-    alert(v1 + v2 + v3 + v4);
+
     
-
-    document.getElementById('v1').innerHTML=v1.Number;
-
-    document.getElementById('v2').innerHTML=v2;
-
-    document.getElementById('v3').innerHTML=v3;
-
-    document.getElementById('v4').innerHTML=v4;
+   pintar();
 
 
 
 }
 
-function vender(v1,v2,v3,v4){
+function vender(){
 
-    v1 = v1 + 2;
+    if(accion>0){
+        accion = accion -1;
+        capital = capital + precio;
+        valor = accion * precio;
+    }
 
-    alert(v1);
-
-    var a1 = Number("v1");
-    var a2 = Number("v2");
-    var a3 = Number("v3");
-    var a4 = Number("v4");
+    pintar();
+   
+}
 
 
-    if(a3>0){
-        a3 = a3 - 1;
-        a2 = a2 + a1;
-        a4 = a3 * a1;
+function pintar(){
+    
+
+    document.getElementById('capital').innerHTML=capital;
+
+    document.getElementById('accion').innerHTML=accion;
+
+    document.getElementById('valor').innerHTML=valor;
+
+}
+
+function pintar_resultado(precio){
+    
+    document.getElementById('precio').innerHTML=precio;
+
+    valor = accion * precio;
+
+    document.getElementById('valor').innerHTML=valor;
+}
+
+function fin(){
+    
+    
+
+    
+    
+    document.getElementById('todo').style.display = 'none'; 
+    total = capital + valor;
+    
+    
+
+    var frase1 = "Felicidades, ha batido el recor con un total de "+total+" euros.";
+    var frase2 = "La suma de su capital y el valor en acciones es de: "+total+" euros.";
+
+
+    if(total > localStorage.getItem("Record")){      
+        
+        
+        document.getElementById('fin').innerHTML='<p>'+frase1+'</p><br><br><img id="copa" src="copa.png"><br><br><button id="repetir" type="submit" onclick="repetir()"> Repetir </button> <button id="salir" type="submit" onclick="salir()"> Salir </button>';
+   
+        localStorage.setItem("Record", total);
+    }else{
+        
+        document.getElementById('fin').innerHTML='<p>'+frase2+'</p><br><br><button id="repetir" type="submit" onclick="repetir()"> Repetir </button> <button id="salir" type="submit" onclick="salir()"> Salir </button>';
     }
 
     
+    
+}
 
-    document.getElementById('v1').innerHTML=a1;
+function repetir(){
+    document.getElementById('fin').innerHTML=''; 
+    document.getElementById('todo').style.display = 'block';  
+    capital = 1000;
+    reinicio();
+}
 
-    document.getElementById('v2').innerHTML=a2;
-
-    document.getElementById('v3').innerHTML=a3;
-
-    document.getElementById('v4').innerHTML=a4;
+function salir(){
+    alert("Gracias por usar el programa, ¡Hasta la próxima!");
+    window.close();
 }
